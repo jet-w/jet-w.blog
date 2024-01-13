1172a8360ed8ccbf60aee24053e1e409286925e5
Haiyue <nutterair1989@gmail.com>
2024-01-13 20:53:21 +1030

    Modify the script for google analytics

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -43,7 +43,7 @@ export default defineUserConfig({
     "@Test": path.resolve(__dirname, "components/test.vue")
   },
   head: [
-    ['srcipt', {src: 'https://www.googletagmanager.com/gtag/js?id=G-4BP2YK8NPN'}],
+    ['srcipt', {"async":"", src: 'https://www.googletagmanager.com/gtag/js?id=G-4BP2YK8NPN'}],
     ['srcipt', {src: '/src/gtag.js'}],
   ]
   // Enable it with pwa
