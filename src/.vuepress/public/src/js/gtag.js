b57f3db410c476b715eb89a027edef0da793cb35
Haiyue <nutterair1989@gmail.com>
2024-01-14 09:29:26 +1030

    add google analytics

--- src/.vuepress/public/src/js/gtag.js
+++ src/.vuepress/public/src/js/gtag.js
@@ -1,7 +1,7 @@
-<script>
+document.addEventListener('DOMContentLoaded', function() {
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
   gtag('config', 'G-DEMNZ1E6YS');
-</script>
+});
